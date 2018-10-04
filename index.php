1a1981183577e87de0e3590ed93f9e39aa2a43b1
harshitdugar <harshitdugar85@gmail.com>
2018-10-04 22:31:06 +0530

    Coming Soon

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Harshit Dugar</title>
+    <link rel="shortcut icon" type="image/x-icon" href="./img/icon.ico">
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/jquery.js"></script>
+    <!--<script src="./js/preloader.js"></script>-->
+  </head>
+  <body>
+    <div class="txt"><p>Harshit<br><br>Dugar</p></div>
+    <div class="txt1"><p>Coming<br><br>Soon</p></div>
+    <div class="spinner-wrapper" id="loader">
+      <div class="spinner">
+        <img src="./img/profile.jpg" height="150" width="150" alt="" id="index-profile">
+      </div>
+    </div>
+
+  </body>
+</html>
