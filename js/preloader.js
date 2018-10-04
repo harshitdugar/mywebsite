8b592a44c4b57daf62173bf205564822e1cc2f9b
harshitdugar <harshitdugar85@gmail.com>
2018-10-05 04:55:35 +0530

    Social links added

--- js/preloader.js
+++ js/preloader.js
@@ -1,17 +1,28 @@
 $(document).ready(function() {
+  $(".social").hide();
+  $(".index-icons").hide();
     //Preloader
-      $(window).on("load", function() {
+      /*$(window).on("load", function() {
         preloaderFadeOutTime = 700;
         function hidePreloader() {
           var preloader = $('.spinner-wrapper');
           preloader.fadeOut(preloaderFadeOutTime);
         }
         hidePreloader();
-      });
+      });*/
     });
 
 var myVar;
-myVar = setTimeout(showPage, 10200);
+myVar = setTimeout(showPage, 4000);
 function showPage() {
     document.getElementById("loader").style.display = "none";
+    document.getElementById("txt").style.display = "none";
+    document.getElementById("txt1").style.display = "none";
+    $(".social").show();
+}
+
+var myVar1;
+myVar1 = setTimeout(showPage1, 6000);
+function showPage1() {
+    $(".index-icons").show();
 }
