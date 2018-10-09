541a6ce2f3778a0c5cd06cb3b94480de28bfdf81
harshitdugar <harshitdugar85@gmail.com>
2018-10-09 22:23:53 +0530

    Final intro done

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  $(".index-about--me").hide();
   $(".social").hide();
   $(".index-icons").hide();
+
     //Preloader
       /*$(window).on("load", function() {
         preloaderFadeOutTime = 700;
@@ -13,16 +15,19 @@ $(document).ready(function() {
     });
 
 var myVar;
-myVar = setTimeout(showPage, 4000);
+myVar = setTimeout(showPage, 2200);
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("txt").style.display = "none";
-    document.getElementById("txt1").style.display = "none";
     $(".social").show();
 }
+//var myVar2 = ((function(){}),3000);
+var myVar2 = setTimeout((function(){
+  $(".index-about--me").show();
+}),5000);
 
 var myVar1;
-myVar1 = setTimeout(showPage1, 6000);
+myVar1 = setTimeout(showPage1, 7000);
 function showPage1() {
     $(".index-icons").show();
 }
